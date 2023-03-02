db5215a40084444735a89fa71fcb2c1aac3f41d8
howecofe <am0229@naver.com>
2023-03-03 01:26:27 +0900

    6-2. 프로덕션 배포: AWS 일래스틱 빈스톡을 이용한 프론트엔드 배포

--- src/api-config.js
+++ src/api-config.js
@@ -4,6 +4,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
+} else {
+    backendHost = "http://prod-todo-api-service-howecofe.ap-northeast-2.elasticbeanstalk.com"; // 일래스틱 빈스톡의 애플리케이션 URL
 }
 
 export const API_BASE_URL = `${backendHost}`;
